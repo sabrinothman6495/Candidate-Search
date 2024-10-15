4d2901909633b84e7421818d7935a37b4ab2970a
sabrinothman95 <68713978+sabrinothman95@users.noreply.github.com>
2024-10-15 14:37:43 -0400

    updatinting vite.config to run app

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,13 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   envDir: './env',
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://api.github.com',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
 });
